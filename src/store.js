bda24bd5eb986921395eb9e05990275885c2eeaf
MxJRd <reed182@mail.chapman.edu>
2020-05-01 13:09:03 -0700

    changes

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 const bookmarks = [
   {
-    id: 1,
+    id: '05995f6e-39dd-442b-8953-9b57c65faeb6',
     rating: '1',
     title: 'title1',
     description: 'description1'
   },
   {
-    id: 2,
+    id: '2a52def1-92eb-4c94-bc03-c08e7f0d2b3e',
     rating: '2',
     title: 'title2',
     description: 'description2'
